aa933c07c9b1db9b183d8c6a7d65782431718ca7
twinbird827 <twinbird827@gmail.com>
2021-04-26 08:40:15 +0900

    iroiro

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ConsoleApp26")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PDF2JPG")]
+[assembly: AssemblyDescription("PDF2JPG")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("_____")]
-[assembly: AssemblyProduct("ConsoleApp26")]
+[assembly: AssemblyProduct("PDF2JPG")]
 [assembly: AssemblyCopyright("Copyright © _____ 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
